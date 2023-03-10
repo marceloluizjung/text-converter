a71a793d0ce5e84b4641ef39e394d772ea469787
Marcelo <marcelo_4940@hotmail.com>
2023-03-09 21:18:00 -0300

    Vai que vai

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4200;
 
 app.use('/', express.static('dist/text-converter'));
 
